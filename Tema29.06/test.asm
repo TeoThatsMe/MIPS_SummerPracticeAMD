f6f4f39ef863a29eedc03b7d718793a47be31e07
TeoThatsMe <iurascuteo@pop-os.localdomain>
2023-07-07 12:56:02 +0300

    UpdateUpdate

--- Tema29.06/test.asm
+++ Tema29.06/test.asm
@@ -1,5 +1,23 @@
-addi $t0, $0,50
-addi $t1,$t0,16
-addi $t1,$t0,32
-addi $t1,$t0,48
-beq $t0,$t0,-3
+main: addi $t0,$0,16
+sw $t0,0($0)
+addi $t0,$t0,16
+sw $t0,1($0)
+addi $t0,$t0,16
+sw $t0,2($0)
+addi $t0,$t0,16
+sw $t0,3($0)
+
+xor $t0,$t0,$t0
+xor $t1,$t1,$t1
+addi $t2,$0,3
+loop_start:
+lw $t3,0($t0)
+add $t1,$t1,$t3
+beq $t0,$t2,loop_end
+addi $t0,$t0,1
+j loop_start
+loop_end:
+addi $t0,$0,100
+slt $t2,$t0,$t1
+and $t2,$t0,$t1
+j main
--- Tema29.06/translator.py
+++ Tema29.06/translator.py
@@ -12,32 +12,92 @@ def getRegVal(reg):
         valRet+=int(reg[2])
     elif "$ra" in reg:
         valRet+=31
-    
+ 
     return valRet
-
-f= open("test.asm","r")
+ 
+f=open("test.asm","r")
 output=open("iesire.hex","w")
 lines=f.readlines()
 linii=[]
+etichete={}
+PC=0
 for l in lines:
     l=l.replace("    ","")
     #l=l.replace(" ","")
     l=l.replace("\n","")
     #   print(l)
-    if l=="" or l.__contains__(':'):
+    if l=="":
+        continue
+    if l.__contains__(':'):
+        #print(PC)
+        temp=l.split(":")
+        etichete[temp[0]]=PC
+        if len(temp[1])==0:
+            continue
+        l=temp[1].replace("    ","")
+        l=l.replace("\n","")
+        l=l.replace(" ","",1)
+        linii.append(l)
+        PC+=1
         continue
     linii.append(l)
-
-
+    PC+=1
+ 
+print(etichete)
+ 
+ 
+counter=1
 #print(linii)
 for linie in linii:
     p1,p2=linie.split(" ",1)
     p2=p2.replace(" ","")
     p2=p2.split(",")
-
+ 
     instructiune=0
-
+ 
     match p1:
+ 
+        #R type
+ 
+        case "add":
+            instructiune+=32
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+        case "sub":
+            instructiune+=34
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+        case "and":
+            instructiune+=36
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+        case "or":
+            instructiune+=37
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+        case "xor":
+            instructiune+=38
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+        case "slt":
+            instructiune+=42
+            instructiune+=(getRegVal(p2[0])<<11)
+            instructiune+=(getRegVal(p2[1])<<21)
+            instructiune+=(getRegVal(p2[2])<<16)
+ 
+ 
+        #I Type
+ 
         case "addi":
             instructiune+=(8<<26)
             instructiune+=int(p2[2])
@@ -45,12 +105,12 @@ for linie in linii:
                 instructiune+=pow(2,16)
             instructiune+=(getRegVal(p2[0])<<16)
             instructiune+=(getRegVal(p2[1])<<21)
-
+ 
         case "lui":
             instructiune+=(15<<26)
             instructiune+=(int(p2[1],0))
             instructiune+=(getRegVal(p2[0])<<16)
-
+ 
         case "sw":
             instructiune+=(43<<26)
             instructiune+=(getRegVal(p2[0])<<16)
@@ -60,7 +120,7 @@ for linie in linii:
             instructiune+=int(imm)
             if(int(imm)<0):
                 instructiune+=pow(2,16)
-        
+ 
         case "lw":
             instructiune+=(35<<26)
             instructiune+=(getRegVal(p2[0])<<16)
@@ -70,44 +130,35 @@ for linie in linii:
             instructiune+=int(imm)
             if(int(imm)<0):
                 instructiune+=pow(2,16)
-
-        case "add":
-            instructiune+=32
-            instructiune+=(getRegVal(p2[0])<<11)
-            instructiune+=(getRegVal(p2[1])<<21)
-            instructiune+=(getRegVal(p2[2])<<16)
-        
-        case "xor":
-            instructiune+=38
-            instructiune+=(getRegVal(p2[0])<<11)
-            instructiune+=(getRegVal(p2[1])<<21)
-            instructiune+=(getRegVal(p2[2])<<16)
-
+ 
         case "beq":
             instructiune+=4<<26
             instructiune+=(getRegVal(p2[0])<<21)
             instructiune+=(getRegVal(p2[1])<<16)
-            if int(p2[2])<0:
+            adresa=etichete[p2[2]]
+            adresa=adresa-counter
+            #print(adresa)
+            if adresa<0:
                 instructiune+=65536
-            instructiune+=int(p2[2])
-
+            instructiune+=adresa
+ 
+ 
+        #J Type
+ 
         case "j":
             instructiune+=2<<26
-            instructiune+=np.uint8(int(p2[0]))
-
-        case "jr":
-            instructiune+=8
-            instructiune+=(getRegVal(p2[0])<<21)
-
+            adresa=etichete[p2[0]]
+            instructiune+=adresa
+            #print(adresa)
+    counter+=1
+ 
     #print(hex(instructiune))
-
-
+ 
+ 
     #print(p2)
     if instructiune!=0:
         print("{0:08x}".format(instructiune))
-        output.write("{0:#010x}".format(instructiune) + "\n")
+        output.write("{0:08x}".format(instructiune) + "\n")
     else:
         print(linie)
         output.write(linie + "\n")
-
-
